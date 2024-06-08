395564588e00ce978d792b555fbf2b70b9ce4b6a
Mostafizur06 <mostafizur1102@gmail.com>
2024-06-08 22:00:31 +0600

    login redirect added

--- user_profile/views.py
+++ user_profile/views.py
@@ -100,9 +100,11 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        # return redirect('login')
+        return redirect('https://mygardenhub.netlify.app/login/')
     else:
-        return redirect('register')
+        # return redirect('register')
+        return redirect('https://mygardenhub.netlify.app/register/')
     
 
 class UserLoginApiView(APIView):
