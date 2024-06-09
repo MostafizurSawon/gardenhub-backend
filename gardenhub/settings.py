6af843ad5c55beb2f773bf2a9c42f98d355e46c4
Mostafizur06 <mostafizur1102@gmail.com>
2024-06-09 11:59:47 +0600

    auth

--- gardenhub/settings.py
+++ gardenhub/settings.py
@@ -65,6 +65,15 @@ INSTALLED_APPS = [
 # }
 
 # REST_FRAMEWORK = {
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.TokenAuthentication',
+#     ],
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+# }
+
+# REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': (
 #         'rest_framework_simplejwt.authentication.JWTAuthentication',
 #     ),
